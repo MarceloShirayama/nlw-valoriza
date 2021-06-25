9d5e02a9d3d620a00684123016b3107510a084b0
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-06-25 16:04:23 -0300

    fix: admin authorization

--- src/middleware/ensureAdmin.ts
+++ src/middleware/ensureAdmin.ts
@@ -1,9 +1,15 @@
 import { Request, Response, NextFunction} from 'express'
+import { getCustomRepository } from 'typeorm';
+import { UsersRepositories } from '../repositories/UsersRepositories';
 
-export function ensureAdmin(request: Request, response: Response, next: NextFunction) {
-  const admin = true;
+export async function ensureAdmin(request: Request, response: Response, next: NextFunction) {
+  const { user_id } = request;
 
-  if (!admin) throw new Error('Unauthorized user! User must be administrator.');
+  const usersRepositories = getCustomRepository(UsersRepositories);
+
+  const user = await usersRepositories.findOne(user_id);
+
+  if (!user?.admin) throw new Error('Unauthorized user! User must be administrator.');
 
   return next();
 }
